7f05ae6febc8355a7b92b3bc438f1d22eddf44f2
mirsamie <mirsamie@PROGRAMMER11>
2020-10-10 14:09:55 +0330

    .

--- class/taapi.js
+++ class/taapi.js
@@ -58,9 +58,9 @@ class TaapiReaderClass {
                     alertCacheLog.result = null
                 }
                 const timeDiff = (new Date() - new Date(alertCacheLog.created_at)) / 60000
-                // console.log(new Date(), alertCacheLog.created_at, new Date(alertCacheLog.created_at))
-                // console.log('TimeDef', timeDiff)
-                // return false
+                console.log(new Date(), alertCacheLog.created_at, new Date(alertCacheLog.created_at))
+                console.log('TimeDef', timeDiff)
+                return false
                 if(timeDiff < parseInt(process.env.ALERT_LIFETIME_MINUTES, 10))
                     return false
             }
