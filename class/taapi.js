4c9710c7e6a98a28ae751398e7f91e882d72875c
mirsamie <mirsamie@PROGRAMMER11>
2020-10-11 13:29:08 +0330

    .

--- class/taapi.js
+++ class/taapi.js
@@ -14,11 +14,12 @@ class TaapiReaderClass {
     }
 
     async getIndicator(indicator, source, symbol, interval, params, backtrack) {
+        source = source.replace(' || ', '')
         console.log(`getIndicator(${indicator}, ${source}, ${symbol}, ${interval})`)
         let client = this.client
         return new Promise(async function(resolve, reject){
             try{
-                let result = await client.getIndicator(indicator, "binance", "BTC/USDT", "4h")//client.getIndicator("bbands2", "binance", "BTC/USDT", "4h")//client.getIndicator(indicator, source, symbol, interval, params, backtrack)
+                let result = await client.getIndicator(indicator, source, symbol, interval)//client.getIndicator("bbands2", "binance", "BTC/USDT", "4h")//client.getIndicator(indicator, source, symbol, interval, params, backtrack)
                 resolve(result)
             }catch(e){
                 reject(e)
