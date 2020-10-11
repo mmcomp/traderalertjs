e64a27d9659a77623b57bfc0f12781f4e64cb748
mirsamie <mirsamie@PROGRAMMER11>
2020-10-11 12:25:19 +0330

    .

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-const TaapiReaderClass = require('./class/taapi')
 const taapi = require("taapi")
 const client = taapi.client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhzY29tcDIwMDJAZ21haWwuY29tIiwiaWF0IjoxNTkxODEyODczLCJleHAiOjc4OTkwMTI4NzN9.iDJhyuqT2_KR_pTzIgmpt5j4VquaApt_G7QpDZWEkrM")
 
-client.getIndicator("bband", "‌‌binance", "BTC/USDT", "15M").then(result => {
-    console.log(result)
-}).catch(err => {
-    console.log(err)
-})
+
+client.getIndicator("bbands2", "binance", "BTC/USDT", "4h").then(function(result) {
+    console.log("Result: ", result);
+});
