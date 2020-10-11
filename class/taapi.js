5ffe2550ebe1dd53a245244b8772c75f5d1a314f
mirsamie <mirsamie@PROGRAMMER11>
2020-10-11 16:04:15 +0330

    .

--- class/taapi.js
+++ class/taapi.js
@@ -51,6 +51,7 @@ class TaapiReaderClass {
                     fs.writeFileSync(cachePath,  dataToStore)
                 }
             }
+            console.log('Result', result)
             alert.result = result
             const alertCacheLog = await AlertCacheLog.query().where('alert_caches_id', alert.id).first()
             if(alertCacheLog){
