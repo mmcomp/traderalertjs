689f3429f005c419418ca3170886f16df747a79f
mirsamie <mirsamie@PROGRAMMER11>
2020-10-19 10:16:41 +0330

    .

--- class/binance.js
+++ class/binance.js
@@ -112,9 +112,9 @@ class BinanceReaderClass {
             // Alert Limit
             let alerts = await AlertLimit.query().withGraphFetched('user')
                 .where('sent', false)
-                .where(function(query) {
-                    query.where('expire_date', '>', currentDate).orWhere('expire_date', null).orWhere('expire_date', '').orWhere('expire_date', '0000-00-00 00:00:00')
-                })
+                // .where(function(query) {
+                //     query.where('expire_date', '>', currentDate).orWhere('expire_date', null).orWhere('expire_date', '').orWhere('expire_date', '0000-00-00 00:00:00')
+                // })
                 .whereIn('currency', selectedCurrencies)
                 
             console.log('Alert Limits', alerts)
