1feb148a7d8b806b653faf258c738d28c3a63396
mirsamie <mirsamie@PROGRAMMER11>
2020-12-06 14:38:51 +0330

    .

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
+/*
 const taapi = require("taapi")
 const client = taapi.client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhzY29tcDIwMDJAZ21haWwuY29tIiwiaWF0IjoxNTkxODEyODczLCJleHAiOjc4OTkwMTI4NzN9.iDJhyuqT2_KR_pTzIgmpt5j4VquaApt_G7QpDZWEkrM")
 
 
 client.getIndicator("fibonacciretracement", "binance", "BTC/USDT", "4h").then(function(result) {
     console.log("Result: ", result);
+});
+*/
+
+
+var axios = require('axios');
+
+axios.get('https://api.taapi.io/rsi', {
+  params: {
+    secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhzY29tcDIwMDJAZ21haWwuY29tIiwiaWF0IjoxNjA3MjQ3Njc0LCJleHAiOjc5MTQ0NDc2NzR9.xcflcz0alaq4aRwHK95FkzsHbg1TiHYIVTjX9o8Vwe0",
+    exchange: "binance",
+    symbol: "BTC/USDT",
+    interval: "1h",
+  }
+})
+.then(function (response) {
+  console.log(response.data);
+})
+.catch(function (error) {
+  console.log(error.response.data);
 });
