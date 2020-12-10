62f90cbf2f6c7c050860e75dfe8354db2d0a90bf
mirsamie <mirsamie@PROGRAMMER11>
2020-12-10 14:55:23 +0330

    .

--- class/taapi.js
+++ class/taapi.js
@@ -59,7 +59,7 @@ class TaapiReaderClass {
         for(var alert of alerts) {
             console.log(` - Start reading an alert from Tapi ${indx} of ${alerts.length} ....`)
             // await this.readAlert(alert)
-            // await new Promise(r => setTimeout(r, parseInt(process.env.TAAPI_REQUEST_INTERVAL, 10)));
+            await new Promise(r => setTimeout(r, parseInt(process.env.TAAPI_REQUEST_INTERVAL, 10)));
             indx++
         }
     }
